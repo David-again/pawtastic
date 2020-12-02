532580e9750a093c9d2b4a985a22daa79f1612de
Seyi <oluseyi.ogunbanjo@gmail.com>
2020-12-01 23:23:43 -0500

    Pawtastic Project - First commit

---
+++ src/component/NavMenus.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class NavMenus extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    return (
+      <div>
+        <img src="../images/logo-pawtastic.jpg" alt="Pawtastic Logo" />
+        PAWTASTIC
+        <p>Human profile</p>
+        <p>Pet basics</p>
+        <p>Pet details</p>
+        <p>Confirm</p>
+      </div>
+    );
+  }
+  //9;
+}
+
+export default NavMenus;
---
+++ src/component/PetRegister.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import NavMenus from "./NavMenus";
+
+class PetRegister extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    return (
+      <div className="page-container">
+        <div className="nav">
+          Something wonderful!
+          <NavMenus />
+        </div>
+        <div className="pet-form"> Something wonderful! </div>
+        {/* <div className="pet-form">Finally </div>
+        <div className="nav">What is this?</div> */}
+      </div>
+    );
+  }
+  //9;
+}
+
+export default PetRegister;
